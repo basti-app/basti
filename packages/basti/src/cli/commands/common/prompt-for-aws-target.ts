e31c5c80f0ba568ddde7d2593a21b469c93079df
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2023-08-29 20:17:06 +0200

    chore(basti): increase aws target prompt page size to 20

--- packages/basti/src/cli/commands/common/prompt-for-aws-target.ts
+++ packages/basti/src/cli/commands/common/prompt-for-aws-target.ts
@@ -34,7 +34,7 @@ export async function promptForAwsTarget(
 
   const { target } = await cli.prompt({
     type: 'list',
-    pageSize: 10,
+    pageSize: 20,
     name: 'target',
     message: 'Select target to connect to',
     choices: [
