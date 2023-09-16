8a66e135eff1cb804240cea74f829ce841b5a9b3
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2023-09-16 10:09:56 +0300

    test: extract jest configuration to the repo root

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-const config = {
+module.exports = {
   preset: 'ts-jest',
 
   roots: ['<rootDir>/src'],
 
   clearMocks: true,
 };
-
-export default config;
